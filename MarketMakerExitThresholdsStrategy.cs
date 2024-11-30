583164dc05f0b88044e088511ddfcea445ffa579
Nasr U Khan <khannasrullah159@gmail.com>
2024-11-30 13:41:40 +0000

    Initial implementation - to be completed - of the market maker hard exit threshold straregy paper

---
+++ MarketMakerExitThresholdsStrategy.cs
@@ -0,0 +1,61 @@
+// C# code for Python implementation of the following paper's methodology:
+// "Market-Maker Hard Exit Thresholds Strategy" by David Shelton, Carlos Veiga
+
+using System;
+
+private chi_minus_1(int h, float nu, int t)
+{ 
+    raise System.NotImplementedException;
+}
+
+private chi_0(int h, float nu, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private chi_1(int h, float nu, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private chi_2(int h, float nu, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private chi(int k, int h, float nu, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private psi(int k, int h, float mu, float lambda_, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private varphi(int k, int h, float mu, float lambda_, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private theta(int x, int y, float mu, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private varrho(int x, int y, float mu, float lambda_, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private sigma(int x, int y, float mu, float lambda_, int t)
+{
+    raise System.NotImplementedException;
+}
+
+private xi(int x, int y, float mu, float lambda_, int t)
+{
+    raise System.NotImplementedException;
+}
+
+// continue implementation
