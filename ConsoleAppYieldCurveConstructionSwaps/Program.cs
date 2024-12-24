8492337e14aa094b1715c8c554604b63863d8f04
Nasr U Khan <khannasrullah159@gmail.com>
2024-12-24 20:02:45 +0000

    Made 2 new console apps for yield curve construction - one for swaps and one for bonds

---
+++ ConsoleAppYieldCurveConstructionSwaps/Program.cs
@@ -0,0 +1,60 @@
+// Yield curve construction using swaps in C#
+
+using System;
+
+// complete implementation
+
+static double _IsHoliday_(DateTime date, string calendar)
+{
+    throw new NotImplementedException();
+}
+
+static double _IsWeekend_(DateTime date)
+{
+    throw new NotImplementedException();
+}
+
+static DateTime _AddBusinessDays_(DateTime date, int numdays, string calendar)
+{
+    throw new NotImplementedException();
+}
+
+static DateTime _AddBusinessMonths_(DateTime date, int nummonths, string calendar)
+{
+    throw new NotImplementedException();
+}
+
+static DateTime _AddBusinessYears_(DateTime date, int numyears, string calendar)
+{
+    throw new NotImplementedException();
+}
+
+static double _YFrac_(DateTime date1, DateTime date2, string daycountconvention)
+{
+    throw new NotImplementedException();
+}
+
+static double[,] __GetSwapCurveData__()
+{
+    throw new NotImplementedException();
+}
+
+static double[,] _DatesForTenors_()
+{
+    throw new NotImplementedException();
+}
+
+static double[,] _YearFractionsForTenors_()
+{
+    throw new NotImplementedException();
+}
+
+static List<double> _SwapYearFractions_(string frequency, int term)
+{
+    throw new NotImplementedException();
+}
+
+static double[,] _ZeroRates()
+{
+    throw new NotImplementedException();
+}
