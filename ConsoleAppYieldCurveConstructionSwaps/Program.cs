1d8eaf03ecef8bc075da2ba77ab982ed68efbfac
Nasr U Khan <khannasrullah159@gmail.com>
2024-12-31 16:02:54 +0000

    Further development of console app for yield curve construction using swaps

--- ConsoleAppYieldCurveConstructionSwaps/Program.cs
+++ ConsoleAppYieldCurveConstructionSwaps/Program.cs
@@ -1,42 +1,121 @@
 // Yield curve construction using swaps in C#
 
 using System;
+using System.Collections.Generic;
 
 // complete implementation
 
-static double _IsHoliday_(DateTime date, string calendar)
+static bool _IsHoliday_(DateTime date, string calendar)
 {
-    throw new NotImplementedException();
+    if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+    {
+        return false;
+    }
+    String dy = date.Day.ToString();
+    String mn = date.Month.ToString();
+    String yy = date.Year.ToString();
+
+    // only implemented NY calendar here - as extension, can implement other calendars
+    if (calendar == "NY")
+    {
+        // Jan 1 bank holiday
+        if ((mn == "1" && dy == "1") || (mn == "12" && dy == "31" && date.DayOfWeek == DayOfWeek.Friday) || (mn == "1" && dy == "2" && date.DayOfWeek == DayOfWeek.Monday))
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
-static double _IsWeekend_(DateTime date)
+static bool _IsWeekend_(DateTime date)
 {
-    throw new NotImplementedException();
+    if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Saturday)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 static DateTime _AddBusinessDays_(DateTime date, int numdays, string calendar)
 {
-    throw new NotImplementedException();
+    DateTime next_date = date;
+    for (int i = 0; i < numdays; i++)
+    {
+        next_date.AddDays(1);
+        while (_IsWeekend_(next_date) || _IsHoliday_(next_date, "NY"))
+        {
+            next_date.AddDays(1);
+        }
+    }
+    return next_date;
 }
 
 static DateTime _AddBusinessMonths_(DateTime date, int nummonths, string calendar)
 {
-    throw new NotImplementedException();
+    DateTime next_date = date;
+    for (int i = 0; i < nummonths; i++)
+    {
+        next_date.AddMonths(nummonths);
+        while (_IsWeekend_(next_date) || _IsHoliday_(next_date, "NY"))
+        {
+            next_date.AddDays(1);
+        }
+    }
+    return next_date;
 }
 
 static DateTime _AddBusinessYears_(DateTime date, int numyears, string calendar)
 {
-    throw new NotImplementedException();
+    DateTime next_date = date;
+    for (int i = 0; i < numyears; i++)
+    {
+        next_date.AddMonths(numyears);
+        while (_IsWeekend_(next_date) || _IsHoliday_(next_date, "NY"))
+        {
+            next_date.AddDays(1);
+        }
+    }
+    return next_date;
 }
 
 static double _YFrac_(DateTime date1, DateTime date2, string daycountconvention)
 {
-    throw new NotImplementedException();
+    if (daycountconvention == "ACTACT")
+    {
+        throw new NotImplementedException();
+    }
+    else if (daycountconvention == "ACT365")
+    {
+        TimeSpan delta = date2.Subtract(date1);
+        double delta_fraction = delta.Days / 365.0;
+        return delta_fraction;
+    }
+    else if (daycountconvention == "ACT360")
+    {
+        TimeSpan delta = date2.Subtract(date1);
+        double delta_fraction = delta.Days / 360.0;
+        return delta_fraction;
+    }
+    else if (daycountconvention == "Thirty360")
+    {
+        throw new NotImplementedException();
+    }
+    else
+    {
+        // default to ACT360 methodology
+        TimeSpan delta = date2.Subtract(date1);
+        double delta_fraction = delta.Days / 360.0;
+        return delta_fraction;
+    }
 }
 
-static double[,] __GetSwapCurveData__()
+static double[,] __GetSwapCurveData__(Dictionary<string, List<object>> curveparams)
 {
     throw new NotImplementedException();
+
 }
 
 static double[,] _DatesForTenors_()
